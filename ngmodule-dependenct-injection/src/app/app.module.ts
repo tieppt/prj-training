2cdcb64aa71fc04c76e802d92b480a29dc60e59b
Tiep Phan <tieppt12@gmail.com>
2018-09-09 22:32:34 +0700

    done lesson 5

--- ngmodule-dependenct-injection/src/app/app.module.ts
+++ ngmodule-dependenct-injection/src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImgSliderModule } from './img-slider/img-slider.module';
+import { ImageGalleryModule } from './image-gallery/image-gallery.module';
+import { ContentProjectionModule } from './content-projection/content-projection.module';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImgSliderModule,
+    ImageGalleryModule,
+    ContentProjectionModule
   ],
-  providers: [],
+  // providers: [AuthService],
+  providers: [
+    {provide: AuthService, useClass: AuthService},
+    {provide: 'API_ENDPOINT', useValue: 'http://api.example.com'},
+    {provide: 'API_URL', useExisting: 'API_ENDPOINT'},
+    {
+      provide: 'some-token',
+      useFactory: function() {
+        return Math.random();
+      }
+    },
+    // {provide: 'API_URL', useValue: 'abc.com'},
+    // {provide: 'API_URL', useExisting: 'api.com'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ngmodule-dependenct-injection/src/app/image-gallery/image-gallery.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-gallery',
+  templateUrl: './image-gallery.component.html',
+  styleUrls: ['./image-gallery.component.scss']
+})
+export class ImageGalleryComponent implements OnInit {
+  listImage = [
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=1',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=2',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=3',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=4'
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
