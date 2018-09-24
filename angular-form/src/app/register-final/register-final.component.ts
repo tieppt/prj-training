c0a036adde1ceebbe7f5286e1a15d67e52e6cc69
Tiep Phan <tieppt12@gmail.com>
2018-09-24 22:48:13 +0700

    update demo regex validation

--- angular-form/src/app/register-final/register-final.component.ts
+++ angular-form/src/app/register-final/register-final.component.ts
@@ -19,12 +19,13 @@ export class RegisterFinalComponent implements OnInit {
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       pwGroup: this.fb.group({
-        password: '',
-        confirmPassword: ''
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
       }, {validator: comparePassword}),
       country: ['', Validators.required],
       age: ['', [Validators.required, Validators.min(18)]],
-      gender: ['', Validators.required]
+      gender: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern(/^\+84\d{9,10}$/)]]
     });
 
     // update form state
